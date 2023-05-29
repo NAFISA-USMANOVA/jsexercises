08dd04d424622cd6da6d326267a0d5248cc6b615
Nafisa <naus7568@gmail.com>
2023-05-29 22:55:59 +0200

    The most of JS exercises is done

---
+++ script.js
@@ -0,0 +1,21 @@
+// 22/05.23 Taller de JS con 
+
+let age = 30
+console.log(age)
+
+const name = "Nafisa"
+
+let isFast = false
+
+
+const demoId = document.querySelector('#demo-id');
+
+let a = 35
+b = 46
+ 
+let groseryList = ["apple", "pineapple", "grapes", "banana", "watermelon"]
+groseryList[3]= "lemon"
+console.log(groseryList)
+
+//DOM manipulation
+
